06dbdc34bf28afd836e4a79575f417a1f84a6c8a
Eduardo Quirino <edu.3532@gmail.com>
2022-03-02 07:36:51 -0300

    Primeiro commit

---
+++ média de quatro notas/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace média_de_quatro_notas
+{
+    partial class FrmCalculaMedia
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCalculaMedia));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.txtn4 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(171)))), ((int)(((byte)(97)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CÁLCULO DA MÉDIA ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSair);
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.lblMedia);
+            this.groupBox1.Controls.Add(this.txtn4);
+            this.groupBox1.Controls.Add(this.txtn3);
+            this.groupBox1.Controls.Add(this.txtn2);
+            this.groupBox1.Controls.Add(this.txtn1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(38, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(461, 202);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.btnSair.Location = new System.Drawing.Point(309, 150);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(109, 32);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.btnLimpar.Location = new System.Drawing.Point(166, 150);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(109, 32);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.btnCalcular.Location = new System.Drawing.Point(18, 150);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(109, 32);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.Location = new System.Drawing.Point(48, 107);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(370, 26);
+            this.lblMedia.TabIndex = 8;
+            this.lblMedia.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtn4
+            // 
+            this.txtn4.Location = new System.Drawing.Point(318, 57);
+            this.txtn4.Name = "txtn4";
+            this.txtn4.Size = new System.Drawing.Size(100, 23);
+            this.txtn4.TabIndex = 7;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(318, 21);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(100, 23);
+            this.txtn3.TabIndex = 6;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(91, 62);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(100, 23);
+            this.txtn2.TabIndex = 5;
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(91, 21);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(100, 23);
+            this.txtn1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "4° Nota";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "3° Nota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "2° Nota";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "1° Nota";
+            // 
+            // FrmCalculaMedia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(527, 320);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmCalculaMedia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo da Média";
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.TextBox txtn4;
+        private System.Windows.Forms.TextBox txtn3;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ média de quatro notas/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace média_de_quatro_notas
+{
+    public partial class FrmCalculaMedia : Form
+    {
+        public FrmCalculaMedia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1, n2, n3, n4, media;
+            n1 = double.Parse(txtn1.Text);
+            n2 = double.Parse(txtn2.Text);
+            n3 = double.Parse(txtn3.Text);
+            n4 = double.Parse(txtn4.Text);
+            media = (n1 + n2 + n3 + n4) / 4;
+
+            if(media >= 7) { MessageBox.Show("Média = " + Math.Round(media, 2) + " -APROVADO!"); }
+            else if (media >= 5) { MessageBox.Show("Média = "+ Math.Round(media,2)+" -EXAME"); }
+                else { MessageBox.Show("Média = " + Math.Round(media, 2) + "- REPROVADO"); }
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtn1.Clear();
+            txtn2.Clear();
+            txtn3.Clear();
+            txtn4.Clear();
+            txtn1.Focus();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            string texto = "Deseja Sair do Programa?";
+            string titulo = "++++ FINALIZANDO ++++";
+            if (MessageBox.Show(texto, titulo, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
